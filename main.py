5ed6d73dc233cd0e227754086508c32a545eb731
LarsPlayz <86638368+LarsPlayz@users.noreply.github.com>
2022-10-18 12:56:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,54 @@
 import time
-from Adafruit_IO import Client
+from Adafruit_IO import *
 import pyfirmata
+import mysql.connector
+from datetime import datetime
 
+
+#kobler til Adafruit dashbordet
 ADAFRUIT_IO_USERNAME = "toot123"
-ADAFRUIT_IO_KEY = "aio_upZF20heIl39iBDvJ0oGj4M8sVRH"
+ADAFRUIT_IO_KEY = "aio_SpwA61R1unmsEAueOY9c3Vp8d1Q6"
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
+#Kobler til MySQL databasen
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="elev",
+  database="adafruitlog"
+)
+mycursor = mydb.cursor()
+
+#Kobler til Arduino
 board = pyfirmata.Arduino('COM4')
 it = pyfirmata.util.Iterator(board)
 it.start()
 
 analog_input = board.get_pin('a:0:i')
 digital = aio.feeds('toggle')
-slider = aio.feeds('lights')
-run_count = 0
 
 while True:
-    #Counter
-    print('Sending count:', run_count)
-    run_count += 1
-    aio.send_data('counter', run_count)
-
-
     #Line Chart
-    chart_data = analog_input.read()
-    print('Sending chart:', chart_data)
+    chart_data = str(analog_input.read())
     aio.send_data('chart', chart_data)
+    print('Sending chart data:', chart_data)
 
+    #Logger data til SQL database
+    now = datetime.now()
+    current_time = str(now.strftime("%d/%m/%Y %H:%M:%S"))
+    sql = "INSERT INTO potentiometer (time, value) VALUES (%s, %s)"
+    mycursor.execute(sql, (current_time, chart_data))
+    mydb.commit()
+    print("Data saved to database")
 
-    #Styrer 5 Lys
     #Tar imot data fra adafruit dashbordet
-    data = aio.receive(digital.key)
-    lights = aio.receive(slider.key)
-    #.key gir en lang liste med ting, så .value leser bare "value" delen
-    lightVal = lights.value
-    #Plusser på 9 fordi bruker port 9-13 på arduinoen
-    lightNum = int(lightVal) + 9
-    print('Light:', lightVal)
+    toggle = aio.receive(digital.key)
     #Knapp blir trykket skrus på alle lys
-    if data.value == "ON":
-        lightNum = 14
+    if toggle.value == "ON":
+        board.digital[8].write(True)
     #Knapp av skrur av igjen lysa
-    elif data.value == "OFF" and lightNum == 9:
-        lightNum = 9
-    #For å skru på lysa
-    for n in range(8, lightNum):
-        board.digital[n].write(True)
-    #For å skru av lysa
-    for n in range(13, lightNum - 1, -1):
-        board.digital[n].write(False)
+    elif toggle.value == "OFF":
+        board.digital[8].write(False)
 
     #Delay for å ungå rate limit
     time.sleep(5)
